25f82bbcae02c77040316b5b54a9afcfbe7ef0dd
LokeshRawat09 <lokeshrawat313@gmail.com>
2024-09-19 04:14:54 +0530

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import  ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './globals.css'; 
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
